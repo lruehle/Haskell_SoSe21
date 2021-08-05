d71ee41f2121c441894a9b51aeda39a8dd37eee2
lruehle <lilly@r3d.de>
2021-08-05 20:20:28 +0300

    create own Random number

--- fileReader.hs
+++ fileReader.hs
@@ -11,6 +11,12 @@ import qualified Data.ByteString.Lazy as BS
 import GHC.Data.Maybe (fromJust)
 import Data.Aeson.Types (parseMaybe)
 import GHC.Tc.Solver.Monad (getInertCans)
+import Data.Time
+import Data.Time.Clock.POSIX
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
+
+
 
 data Person = Person {
     name :: String
@@ -66,6 +72,8 @@ contents :: IO String
 contents = readFile file
 
 
+
+
 -- if delimiter needed use strListToStringWith
 strListToString :: [String] -> String -- ["erster Satz","zweiter Satz linebreak","dritter Satz"] -- unlines does same check: https://hackage.haskell.org/package/base-4.15.0.0/docs/Prelude.html#v:unlines
 strListToString [] = []
@@ -98,6 +106,26 @@ fallOverAndDie err = do putStrLn err
                         exitWith (ExitFailure 1)
 
 
+date :: UTCTime -> (Integer,Int,Int) --Year is type Integer
+date = toGregorian .utctDay -- will output f.e (2021,8,5)
+
+
+
+
+
+randomNumber :: (Integer,Int,Int) -> UTCTime ->Int
+--randomNumber (y,m,d) time = fromIntegral(diffTimeToPicoseconds (utctDayTime time)) + m + d + fromIntegral y
+randomNumber (y,m,d) time = div (a + b + c + e ) 2
+    where a = fromIntegral(diffTimeToPicoseconds (utctDayTime time))
+          b =  d
+          c =  m
+          e =  fromIntegral y
+{--
+get day, get Time, get time since runghc started (or other "random" values)
+put into Range of 0, getLength (Gedichte Gedicht)
+--}
+
+
 main :: IO ()
 main = do --https://www.schoolofhaskell.com/school/starting-with-haskell/libraries-and-frameworks/text-manipulation/json
     --content <- contents
@@ -105,7 +133,8 @@ main = do --https://www.schoolofhaskell.com/school/starting-with-haskell/librari
     --print contents -- doesn't work because of ´unopenable´ IO Box? IO is not function, but action. So we need to run it before printing. <- basically is "run" operator 
     -- print jsonDate 
     --parsedData <- (eitherDecode <$> jsonData) :: IO (Either String [Gedicht]) --gedichte3.json
-
+    current <- getCurrentTime -- not TimeZoned
+    --let PicosecondsSince = diffTimeToPicoseconds (utctDayTime current) --10^-12 for Pico to second
     parsedData <- eitherDecode <$> jsonData :: IO (Either String Gedichte) -- without IO throws error; reminder <$> is short for fmap, check on Functors
     case parsedData of --https://stackoverflow.com/questions/46944347/how-to-get-value-from-either   otherwise will return "Left ..." or "Right..."
        Left err -> fallOverAndDie ("Parsing Error! Programm will be stopped because: "++err)
@@ -115,23 +144,32 @@ main = do --https://www.schoolofhaskell.com/school/starting-with-haskell/librari
        --Right parsed -> print ( getIntro (gedichte parsed!!1) ++ show(getLength (gedichte parsed))) -- prints getIntro for (gedichte (parsed index 1)) -- remember index 0 is head
        Right parsed -> do 
            putStrLn "parsing done" --print $ printRandGedicht parsed -- put into variable but NOT print possible?
-           putStrLn "Wilkommen in der Gedichte Sammlung"
-           putStrLn  ("Zur Zeit stehen " ++ show (getLength(gedichte parsed)) ++ " Gedichte zur Auswahl")
-           putStrLn "Möchten Sie ein zufälliges Gedicht lesen?"
+           putStrLn ("Wilkommen in der Gedichte Sammlung! \n Zur Zeit stehen " ++ show (getLength(gedichte parsed)) ++ " Gedichte zur Auswahl \n")
+           putStrLn ("Sekunden seit Mittnacht: "++ show current)  
+           putStrLn ("Sekunden seit Mittnacht: "++ show (diffTimeToPicoseconds (utctDayTime current)))  
+           putStrLn ("random Number "++ show (randomNumber (date current) current))  
+           putStrLn ("datum ist: "++ show (date current))  
+           putStrLn "Möchten Sie ein zufälliges Gedicht lesen? (y/n)"
            user <- getLine
-           let check | user == "y" = putStr ("Super! \n" ++ printRandGedicht parsed)
+           let check | user == "y" = putStr ("Super, viel Spaß! \n" ++ printRandGedicht parsed)
                      | user == "n" = putStr "Sehr Schade, Sie verpassen was"
                      | otherwise = putStr "Ich lerne noch! Bisher verstehe ich nur 'y' für ja und 'n' für nein"
            check
            
 
 
+
 {-- next up:
-    xxx get length of Gedicht -> Gedichte
     make random number from length
-    xxx print out Data at length
-    print out more Data than just Titel & Autor
+        ***-> make own random number
+        -> make random number with Range of Length
     Change read 
+    make own "Date" - Type?
+    repeat questions in main (without repeating parse possible? reasonable? Repeat only right? Reasonable until poems can get added?)
+    print choosable options to cmd possible? 
+    xxx get length of Gedicht -> Gedichte
+    xxx print out Data at length
+    xxx print out more Data than just Titel & Autor
     xxx think about Poem formatting / reading / printing ; seperate by ,.;:? use \n ? no new line in JSON source accepted 
             for human readableness use List of Strings for Lines?
 --} 
